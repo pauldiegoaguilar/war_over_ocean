0a7c05977da634e720974d5d8dfca03d277ee21e
pauldiegoaguilar <diego.paul.et.n26@gmail.com>
2023-05-04 08:46:33 -0300

    AVANCE-NO-TERMINADO-MECANICA-MISIL-DIVISOR

---
+++ Assets/Scripts/HabilitiesManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HabilitiesManager : MonoBehaviour
+{
+    public GameObject misil;
+
+    void Update(){
+        if(Input.GetKeyUp(KeyCode.Q)){
+            crearMisil();
+        }
+    }
+
+    public void OnButtonPress(){
+        crearMisil();
+    }
+
+    void crearMisil(){
+        Instantiate(misil, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/MisilDivisorBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MisilDivisorBehavior : MonoBehaviour
+{
+
+    public Transform target;
+    Vector3 direction;
+    float speed = 20;
+    float rotationSpedd = 5;
+
+    void Start() {
+        target = GameObject.Find("Barco 2").transform;
+        transform.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * 7f, ForceMode.Impulse);
+    }
+
+    void Update() {
+        // Movement
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    
+        // Rotation
+        if(target != null){
+            direction = target.position - transform.position;
+
+            direction = direction.normalized;
+            var rot = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, rotationSpedd * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if(col.CompareTag("Player")){
+            Destroy(gameObject);
+        }
+    }
+
+}
