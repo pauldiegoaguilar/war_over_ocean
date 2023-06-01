d90e14fb6e7a2d79ac7c1c7767b75544a869d477
Matias-M-ETN26 <matiasmonzonetn26@gmail.com>
2023-06-01 11:35:37 -0300

    porfavor paul diego no borres mi tares😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭

---
+++ Assets/Scripts/regenerar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class regenerar : MonoBehaviour
+{
+    public GameObject ships;
+    public bool verificacion;
+
+    private void Update()
+    {
+        // Detectar clic del mouse
+        if (Input.GetMouseButtonDown(0) && verificacion == true)
+        {
+            // Obtener el objeto clickeado
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject objetoClickeado = hit.collider.gameObject;
+                Debug.Log(objetoClickeado.name);
+                if(objetoClickeado.tag == "destruido" && objetoClickeado.transform.IsChildOf(ships.transform)){
+                    objetoClickeado.tag = "normal";
+                    verificacion = false;
+                    Destroy(gameObject);    
+                }    
+            }
+        }
+    }
+    public void regenerarVerif(){
+        Debug.Log("boton funcionando");
+        verificacion = true;
+    }
+
+}
